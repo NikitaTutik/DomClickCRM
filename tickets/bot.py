7f56fab242182ec6c73bc2565f487ca9f2052203
NikitaTutik <famounst@gmail.com>
2021-07-24 18:32:04 +0300

    requirements

--- tickets/bot.py
+++ tickets/bot.py
@@ -3,8 +3,8 @@ from django.conf import settings
 
 
 def send_msg(text):
-   token = '1842995007:AAGgpe5ivziqOy5DLWJNx41Euq1Xfq6rjJ8'
-   chat_id = "-583457919"
+   token = settings.BOT_API_KEY
+   chat_id = settings.BOT_CHAT_ID
    url_req = "https://api.telegram.org/bot" + token + "/sendMessage" + "?chat_id=" + chat_id + "&text=" + text
    send = requests.get(url_req)
 
